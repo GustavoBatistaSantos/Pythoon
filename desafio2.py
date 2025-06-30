9633bd57edc798c3ba049a297418ed6acf34eb80
gustavo1506 <145714578+gustavo1506@users.noreply.github.com>
2025-06-30 11:04:22 -0300

    mnb

---
+++ desafio2.py
@@ -0,0 +1,14 @@
+nome = input ('Qual é o seu nome? ')
+print ('bem vindo {}! '.format(nome))
+
+dia = input('dia')
+mes = input('mês')
+ano = input('ano')
+
+print ('Você nasceu no dia '+ dia + ' de '+ mes + ' de '+ ano + '.Correto?')
+
+num1 = int (input('Primeiro número'))
+num2 = int (input('Segundo número'))
+soma = int (num1+num2)
+
+print('a soma é', soma)
