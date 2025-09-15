69d35b9abb5f05bfd69479985a3643fe56b2a307
gustavo1506 <145714578+gustavo1506@users.noreply.github.com>
2025-09-14 21:00:46 -0300

    lkjk

--- aula4.py
+++ aula4.py
@@ -3,5 +3,18 @@
 # **
 # * / // %
 # + - #
-nome = input ('Qual é o seu nome?')
-print('prazer em conhece-lo {:^20}!'.format(nome))
+#nome = input ('Qual é o seu nome?')
+#print('prazer em conhece-lo {:^20}!'.format(nome))
+
+n1 = int(input('um valor'))
+n2 = int(input('outro valor'))
+s = n1+n2
+m = n1*n2
+d= n1/n2
+di = n1//n2
+e = n1**n2
+print('A soma é {}, \n o produto é {},\n e a divisão é {:.3f} '.format(s,m,d), end='')
+print (' divisão inteira {} e exponenciação {}'.format(di,e))
+#:.3f é 3 casas flutuantes
+#end='' é para não quebrar a linha
+#\n para quebrar a linha no meio do print
---
+++ desafio3.py
@@ -0,0 +1,2 @@
+n1= int(input('um valor'))
+print('o número digitado foi{} sendo o seu sucessor {} e seu antecessor {}'.format(n1, (n1+1),(n1-1)))
