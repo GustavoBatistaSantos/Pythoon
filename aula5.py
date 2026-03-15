3c02e9decb333d37acca02b51fab0264715c1e57
Gustavo Batista <145714578+gustavo1506@users.noreply.github.com>
2026-03-15 10:26:54 -0300

    hgc

---
+++ aula5.py
@@ -0,0 +1,13 @@
+import math
+num = int(input('Digite um número: ' ))
+raiz = math.sqrt(num)
+#print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz))) arredonda pra cima
+#print('a raiz de {} é igual a {}'.format(num, math.floor(raiz)))
+
+#importando funcionalidades específicas 
+
+#from math import sqrt, floor, ceil
+#num = int(input('Digite um número: ' ))
+#raiz = sqrt(num)
+#print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz))) arredonda pra cima
+#print('a raiz de {} é igual a {}'.format(num, floor(raiz)))
