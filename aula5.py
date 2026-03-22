8d4d2d4ece9a1a73518fe23dbbebaefab96cfcde
Gustavo Batista <145714578+gustavo1506@users.noreply.github.com>
2026-03-21 22:16:16 -0300

    zc

--- aula5.py
+++ aula5.py
@@ -1,6 +1,6 @@
-import math
-num = int(input('Digite um número: ' ))
-raiz = math.sqrt(num)
+#import math
+#num = int(input('Digite um número: ' ))
+#raiz = math.sqrt(num)
 #print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz))) arredonda pra cima
 #print('a raiz de {} é igual a {}'.format(num, math.floor(raiz)))
 
@@ -10,3 +10,14 @@ raiz = math.sqrt(num)
 #raiz = sqrt(num)
 #print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz))) arredonda pra cima
 #print('a raiz de {} é igual a {}'.format(num, floor(raiz)))
+
+#importando a biblioteca ramdom 
+#import random
+#num = random.random()#computador vai gerar um número aleatório, entre 0 e um
+#num2 = random.randint(1,10)
+#print(num)
+#print(num2)
+
+#importando bibliotecas de emojis
+import emoji
+print(emoji.emojize("Olá, mundo 😎"))
