96fe7062a2715083f7ff0e454a4ec4119499d1e0
Gustavo Batista <145714578+gustavo1506@users.noreply.github.com>
2026-05-12 08:13:27 -0300

    hggf

--- desafio37.py
+++ desafio37.py
@@ -1,5 +1,5 @@
-n1 = float ("Digite a primeira nota")
-n2 = float ("Digite a segunda nota")
+n1 = float (input("Digite a primeira nota"))
+n2 = float (input ("Digite a segunda nota"))
 media = (n1+n2)/2
 if media < 5:
     print("Reprovado")
