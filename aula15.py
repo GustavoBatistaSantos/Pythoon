3e127ac8d66c2e9b4857b7de50897eefabaad7a4
Gustavo Batista <145714578+gustavo1506@users.noreply.github.com>
2026-08-31 20:45:19 -0300

    DSF

---
+++ aula15.py
@@ -0,0 +1,17 @@
+# teste = []
+# teste.append('gustavo')
+# teste.append(30)
+# galera = []
+# #galera.append(teste)
+# galera.append(teste[:])
+# teste[0] = 'Maria'
+# teste[1] = 22
+# #galera.append(teste)muda a lista de cima pois eu estou ligando as listas
+# galera.append(teste[:]) #agora ele ta fazendo uma copia e não ligando as listas
+# print(teste)
+# print(galera)
+
+galera = [['Joao',19],['Ana',33],['Joaquim', 13],['Maria',45]] #declaração de 4 listas dentro da principal.
+print(galera)
+
+ 
