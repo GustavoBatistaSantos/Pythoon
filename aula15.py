7d06740421fc0ed229d5cf207c599d6b2338ba80
Gustavo Batista <145714578+gustavo1506@users.noreply.github.com>
2026-09-01 17:27:49 -0300

    sgd

--- aula15.py
+++ aula15.py
@@ -11,7 +11,28 @@
 # print(teste)
 # print(galera)
 
-galera = [['Joao',19],['Ana',33],['Joaquim', 13],['Maria',45]] #declaração de 4 listas dentro da principal.
+# galera = [['Joao',19],['Ana',33],['Joaquim', 13],['Maria',45]] #declaração de 4 listas dentro da principal.
+# print(galera[2][1])
+# for p in galera:
+#     print(f'{p[0]} tem {p[1]} anos de idade.')
+#     # print(p[1]) para mostrar apenas a idade
+
+galera = []
+dado = []
+tomai = tomen = 0
+for c in range(0,3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera.append(dado[:])
+    #galera.append(dado) assim vai apagar as duas listas porque estão conectadas 
+    dado.clear()
 print(galera)
 
- 
+for p in galera:
+    if p[1] >=18:
+        print(f'{p[0]} é maior de idade')
+        tomai += 1
+    else:
+        print(f'{p[0]} é menor de idade')
+        tomen += 1
+print(f'Temos {tomai} maiores de idade e {tomen} menores de idade')
---
+++ desafio81.py
@@ -0,0 +1,5 @@
+pessoas = []
+nome = []
+peso = []
+for p in pessoas (0,3):
+    
